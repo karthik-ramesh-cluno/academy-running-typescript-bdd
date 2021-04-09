b0beac14eb2e8f14a706daab1b1a5593ab43d363
Vittorio Guerriero <vittorio.gue@gmail.com>
2021-04-09 15:30:44 +0100

    mob next [ci-skip]

--- src/specs/features/shopping-basket.feature
+++ src/specs/features/shopping-basket.feature
@@ -5,4 +5,9 @@ Feature: Shopping Basket
     Given I add 2 units of "The Hobbit" to my shopping basket
     And I add 5 units of "Breaking Bad"
     When I check the content of my shopping basket
-    Then it should contain the following information
+    Then it should have been created on the "2020-12-01"
+    And it should contain the following items
+      | Item            | Quantity  | Price | Total Price |
+      | 'The Hobbit'    | 2         | 5.00  | 10.00       |
+      | 'Breaking Bad'  | 5         | 7.00  | 35.00       |
+    And the cart total is "£45.00"
--- src/specs/step-definitions/basket.steps.ts
+++ src/specs/step-definitions/basket.steps.ts
@@ -1,29 +1,53 @@
-import {DataTable} from '@cucumber/cucumber'
-import {defineFeature, loadFeature} from "jest-cucumber";
+import { defineFeature, loadFeature } from "jest-cucumber";
 
 const feature = loadFeature('./src/specs/features/shopping-basket.feature')
 
-defineFeature(feature, test => {
+class Basket {
 
-//     const mockPrintline = jest.fn()
-//     const printer = {
-//         printline: mockPrintline
-//     }
-//     let account: Account
-//     beforeEach(() => {
-//         account = new Account(printer)
-//     })
-// 
-    test("Add items to basket", ({ given, and, when, then}) => {
-      given(/I add (\d+) units of "(.*)" to my shopping basket/, (quantity: number, product: string) => {
-        basket.addProduct(product, quantity)
-      })
-      and(/I add (\d+) units of "(.*)"/, (quantity: number, product: string) => {
-        basket.addProduct(product, quantity)
-      })
-      when('I check the content of my shopping basket', () => {})
-      then('it should contain the following information', () => {})
+    addProduct(product: string, quantity: number) {
+        throw new Error('notp implemented')
+    }
+
+    getCreationDate() {
+        return undefined;
+    }
+
+    getItems() {
+        return undefined;
+    }
+
+    getTotal() {
+        return undefined;
+    }
+}
+
+defineFeature(feature, test => {
+    let basket: Basket;
+    beforeEach(() => {
+        basket = new Basket()
+    })
+
+    test("Add items to basket", ({given, and, when, then}) => {
+        given(/I add (\d+) units of "(.*)" to my shopping basket/, (quantity: number, product: string) => {
+            basket.addProduct(product, quantity)
+        })
+        and(/I add (\d+) units of "(.*)"/, (quantity: number, product: string) => {
+            basket.addProduct(product, quantity)
+        })
+        when('I check the content of my shopping basket', () => {
+
+        })
+        then(/^it should have been created on the "(.*)"$/, (date) => {
+            expect(basket.getCreationDate()).toBe(date)
+        })
+        then(/^it should contain the following items$/, (printedOutput) => {
+            expect(basket.getItems()).toBe(printedOutput)
+        })
+        then(/^the cart total is "([^"]*)"$/, (total) => {
+            expect(basket.getTotal()).toBe(total)
+        })
     })
 })
+
 
 
